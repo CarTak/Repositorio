ac949f453547a5849e3bba8d0470a91ea1983648
CarTak <cartakkun@gmail.com>
2022-09-15 18:13:41 -0300

    Primeiro Programa em Java

---
+++ index.js
@@ -0,0 +1,27 @@
+/**
+ * -> Tipos primitivos
+ * -> Variaveis no JavaSript
+ * -> Operadores matemáticos do JavaSript
+ */
+
+// serve para escrever mensagens no console
+
+// string -> textos
+// number -> numeros
+// boolean -> booleano
+
+console.log("Hello World")
+console.log(5)
+console.log(43.76)
+console.log(true)
+console.log(false)
+
+// variaveis
+let mensagem = 'SoulCode BCW25 JFS' 
+
+console.log(mensagem) //Declaração de variavel
+
+mensagem = "Novo Texto"
+
+console.log(mensagem)
+
